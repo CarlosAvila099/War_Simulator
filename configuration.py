fc3337131a9f20ec6d7f4254b65b270806aaef0d
Carlos Daniel Avila Navarro <carlosavila6274@gmail.com>
2022-05-21 23:56:02 -0500

    Ordered Configurations.

--- configuration.py
+++ configuration.py
@@ -1,13 +1,13 @@
-LUCK = 10                       # The probability that luck boost will be activated. Must be between 0 and 100
-BOOST = 1.3                     # The luck multiplier.
-GROWTH_TIME = 1                 # Number of months required for a Continent to grow in population and income.
+DURATION = 1000                 # The days that the simulation will run.
+LAND_SIZE = 100000              # The size of each part of Land.
 GOVERNMENT_TIME = 5             # Number of years required for a Continent to change government rate.
 PEACE_TIME = 10                 # Number of years required for a Continent to grow due to peace.
-LAND_SIZE = 100000              # The size of each part of Land.
-INCOME_THRESHOLD = .5           # The threshold that create an income trigger for an Inner Battle.
-INSURGENCY_SUPPORT = 0.2        # An extra to the government rate when the insurgents win.
-DURATION = 1000                 # The days that the simulation will run.
-CASUALTY_STOP = 40              # The percentaje of casualties needed to completely stop a Civil War.
-STALE_TIME = 15                 # The number of days needed for a Stale State to end a War.
+INCOME_THRESHOLD = 0.5           # The threshold that create an income trigger for an Inner Battle.
+LUCK = 10                       # The probability that luck boost will be activated. Must be between 0 and 100.
+BOOST = 1.3                     # The luck multiplier.
+SOCIAL_SUPPORT = 2              # A multiplier that increases luck boost probability to insurgents in a Civil War.
+CASUALTY_STOP = 40              # The percentaje of casualties needed to completely stop a Civil War. Must be between 0 and 100.
+INSURGENCY_SUPPORT = 0.2        # An extra to the government rate when the insurgents win the Civil War.
+STALE_TIME = 15                 # The number of battles needed for a Stale State to end a War.
 
-MONTHS = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
+MONTHS = [31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
