7b20302f70416a17939ea1feb1db21b291dff7b6
Carlos Daniel Avila Navarro <carlosavila6274@gmail.com>
2022-05-21 22:24:06 -0500

    Updated configuration.py comments

--- configuration.py
+++ configuration.py
@@ -4,10 +4,10 @@ GROWTH_TIME = 1                 # Number of months required for a Continent to g
 GOVERNMENT_TIME = 5             # Number of years required for a Continent to change government rate.
 PEACE_TIME = 10                 # Number of years required for a Continent to grow due to peace.
 LAND_SIZE = 100000              # The size of each part of Land.
-INCOME_THRESHOLD = .5           # The threshold that create an income trigger for an intern conflict.
+INCOME_THRESHOLD = .5           # The threshold that create an income trigger for an Inner Battle.
 INSURGENCY_SUPPORT = 0.2        # An extra to the government rate when the insurgents win.
 DURATION = 1000                 # The days that the simulation will run.
 CASUALTY_STOP = 40              # The percentaje of casualties needed to completely stop a Civil War.
-STALE_TIME = 15                 # The number of days needed for a stale state to end a conflict.
+STALE_TIME = 15                 # The number of days needed for a Stale State to end a War.
 
 MONTHS = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
