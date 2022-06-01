99209befe86a0f01778afa98bb1ef909f014db39
PuPunPa <josepablolppl@gmail.com>
2022-06-01 12:34:17 -0500

    Created remaning Stories

--- war_simulation.py
+++ war_simulation.py
@@ -9,7 +9,7 @@ from configuration import DURATION, STORY_SET
 
 from pprint import pprint
 
-
+'''
 # STORY 1: NORMAL WORLD (Geographically acurrate. Normal data, without changes)
 world = World(85,  [
                         Continent("North America", 5, 7, 30, 24710000, 580000000, 1, 50984.45, 88, 3.4, random.random()),
@@ -20,7 +20,7 @@ world = World(85,  [
                         Continent("South-East Asia", 60, 41, 180, 11840000, 680000000, 1.14, 4523.84, 70, 2.70, random.random()),
                         Continent("Oceania", 68, 57, 210, 8526000, 41000000, 1.4, 46209.11, 66, 2.10, random.random()),
                     ])
-
+'''
 '''
 # STORY 2: AFRICA CENTER (All around Africa. Increase in Income and Military Spend of As [1969.69 --> 12354.2][1.76 --> 6.76])
 #  - Values are adjusted so that NA and As can go to war with Af, Af can't defend itself from them, but Af can attack the other continents. 
@@ -49,6 +49,7 @@ world = World(85,  [
                     Continent("South-East Asia", 60, 41, 180, 11840000, 680000000, 0.6, 34723.84, 70, 2.70, random.random()),
                     Continent("Oceania", 68, 57, 210, 8526000, 41000000, 0.7, 86209.11, 66, 2.10, random.random()),
                     ])
+
 '''
 '''
 # STORY 4: SWITCH OF POWER (New arrangement of continents. The Income of NA and As goes down, while Income of SA, Eu and S-EA goes up). 
@@ -62,7 +63,7 @@ world = World(85,  [
                     Continent("South-East Asia", 67, 40, 180, 11840000, 680000000, 1.14, 28523.84, 70, 2.70, random.random()),
                     Continent("Oceania", 61, 56, 210, 8526000, 41000000, 1.4, 46209.11, 66, 2.10, random.random()),
                     ])
-'''
+
 '''
 # STORY 5: OCEANIA CONQUEST (Geographically acurrate. Increase Growth, Income and Military Spend in O 
 #   - [1.4 --> 8.4][46209.11 --> 356209.11][2.10 --> 102.10])
@@ -75,7 +76,7 @@ world = World(85,  [
                     Continent("South-East Asia", 60, 41, 180, 11840000, 680000000, 1.14, 4523.84, 70, 2.70, random.random()),
                     Continent("Oceania", 68, 57, 210, 8526000, 41000000, 8.4, 356209.11, 98, 102.10, random.random()),
                     ])
-'''
+
 def update_im(i, img, world: World):
     history.append(json_array(world.get_array()))
     world.advance()
@@ -168,6 +169,6 @@ else:
                 f"\t\tIncome: {continent.income[day]}\n"
             )
     
-    file = open("Stories/Story1.txt", "w")
+    file = open("Stories/Story5.txt", "w")
     file.write(text)
     file.close()
