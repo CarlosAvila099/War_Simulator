4480a3f5e057d6d02257b3521a36bcddd8504be0
Carlos Daniel Avila Navarro <carlosavila6274@gmail.com>
2022-05-31 19:49:08 -0500

    Added json_array() and added comments.

--- assets.py
+++ assets.py
@@ -1,7 +1,15 @@
 import math
 from configuration import *
 
-def get_date(days):
+def get_date(days: int):
+    """Formats a number of days into a date.
+
+    Args:
+        days (int): The number of days.
+
+    Returns:
+        str: The date in str format.
+    """
     years = int(days/365)
     days = days - (365*years)
     for i, x in enumerate(MONTHS):
@@ -21,3 +29,21 @@ def std(array: list):
     """
     mean = sum(array) / len(array)
     return math.sqrt( (sum(abs(a - mean)**2 for a in array)) / len(array) )
+
+def json_array(array: list):
+    """Generates a JSON string from an array as a grid, containing only the values of the cell.
+
+    Args:
+        array (list): The array to be formatted.
+
+    Returns:
+        str: The str containing the array in JSON format.
+    """
+    json = ""
+    for line in array:
+        json += "["
+        for pixel in line:
+            json += f"{pixel},"
+        json = json[:-1] + "],"
+    json = json[:-1]
+    return json
